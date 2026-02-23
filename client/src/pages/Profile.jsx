b046d9c6b95e8e80a9129d7458e45d369a0692ba
Deepak <deepakharagalli@gmail.com>
2026-02-23 23:07:20 +0530

    profile page media posts tabs are implemented

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,10 +2,13 @@ import React, { useEffect,useState } from 'react';
 import { dummyPostsData, dummyUserData } from '../assets/assets'
 import Loading from '../components/Loading'
 import UserProfileinfo from '../components/UserProfileinfo'
+import PostCard from '../components/PostCard'
 
 
 // useParams is used to get the params from the url
 import {useParams} from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import moment from 'moment';
 
 const Profile = () => {
 
@@ -40,10 +43,65 @@ const Profile = () => {
           </div>
           {/* user info */}
           <UserProfileinfo user={user} posts={posts} profileId={profileId} setShowEdit={setShowEdit}  />
+        </div>
 
+        {/* {Tabs} */}
+        <div className='mt-6'>
+          <div className='bg-white rounded-xl shadow p-1 flex max-w-md mx-auto'>
+            {
+              ["posts","media","likes"].map((tab)=>(
+                <button 
+                onClick={()=>setActiveTab(tab)}
+                key={tab}
+                className={`flex-1 px-4 py-2 text-sm font-medium rounded-lg transition-colors cursor-pointer ${activeTab === tab ? "bg-indigo-600 text-white" : "text-gray-600 hover:text-gray-900"}`}
+                >
+                  {tab.charAt(0).toUpperCase() + tab.slice(1)}
+                </button>
+              ))
+            }
+          </div>
+          {/* {posts } */}
+          {
+            activeTab == 'posts' && (
+              <div className='mt-6 flex flex-col items-center gap-6'>
+                {posts.map((post)=> <PostCard key={post._id} post={post}/>)}
+              </div>
+
+            )
+
+          }
+          {/* {media} */}
+          {
+            activeTab === 'media' && (
+              <div className='flex flex-wrap mt-6 max-w-6xl'>
+                {
+                  posts.filter((post)=>post.image_urls.length > 0).map((post)=>(
+                    <>
+                    {
+                      post.image_urls.map((image,index)=>(
+                        <Link target='_blank' to={image} key={index} className='relative group' >
+                        <img key={index} src={image} alt="" className='w-64 aspect-video object-cover' />
+                        <p 
+                        className='absolute bottom-0 right-0 text-xs p-1 px-3 backdrop-blur-xl  text-white opacity-0 group-hover:opacity-100 transition duration-300'>
+                          Posted {moment(post.createdAt).fromNow()}</p>
+
+                        </Link>
+                      ))
+                    }
+
+                    </>
+                  ))
+                }
+
+              </div>
+
+            )
+          }
 
         </div>
       </div>
+      {/* {Edit profile model} */}
+      {showEdit && <p>Show Profile Edit</p>}
       
     </div>
   ) 
